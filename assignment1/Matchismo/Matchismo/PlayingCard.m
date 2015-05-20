a79d7d999bb49c5ed73868c5fb71792fb64db52b
Donald Steinert <steinert.donald@gmail.com>
2015-05-20 15:28:00 -0400

    added Playing Card class

---
+++ assignment1/Matchismo/Matchismo/PlayingCard.m
@@ -0,0 +1,62 @@
+//
+//  PlayingCard.m
+//  Matchismo
+//
+//  Created by Donald Steinert on 5/20/15.
+//  Copyright (c) 2015 Donald Steinert. All rights reserved.
+//
+
+#import "PlayingCard.h"
+
+@implementation PlayingCard
+
+// needed because we use custom getter and setter methods
+@synthesize suit = _suit;
+
+// valid ranks
++ (NSArray *)rankStrings
+{
+    return @[@"?", @"A", @"2", @"3", @"4", @"5", @"6", @"7", @"8",
+      @"9", @"10", @"J", @"Q", @"K"];
+}
+
+// maximum ranking
++ (NSUInteger)maxRank
+{
+    return [[self rankStrings] count] - 1;
+}
+
+// valid suits
++ (NSArray *)validSuits
+{
+    return @[@"♠︎", @"♣︎", @"♥︎", @"♦︎"];
+}
+
+// custom getter for inhereted contents property to display rank and suit
+- (NSString *)contents
+{
+    NSArray *rankStrings = [PlayingCard rankStrings];
+    return [rankStrings[self.rank] stringByAppendingString:self.suit];
+}
+
+- (void)setSuit:(NSString *)suit
+{
+    if ([[PlayingCard validSuits] containsObject:suit]) {
+        _suit = suit;
+    }
+}
+
+// custom getter for suit to return "?" if card has suit nil
+- (NSString *)suit
+{
+    return _suit ? _suit : @"?";
+}
+            
+// custom setter for rank to ensure rank is a proper value
+- (void)setRank:(NSUInteger)rank
+{
+    if (rank <= [PlayingCard maxRank]) {
+        _rank = rank;
+    }
+}
+@end
